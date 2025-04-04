90c7bb527e0134f60d8deaed9e1b2e1ed57f29e3
Shweta Karne <ShwetaKarne84@gmail.com>
2025-04-03 17:06:12 -0700

    Added code to validate address

--- AmazingBooks-UI/src/app/services/shippo-api.service.ts
+++ AmazingBooks-UI/src/app/services/shippo-api.service.ts
@@ -8,25 +8,7 @@ import { Address } from '../model/address';
 export class ShippoApiService {
   baseUrl = 'https://localhost:7186/api/Addresses';
 
-  constructor(private http: HttpClient) {
-    /* httpOptions = {
-    headers: new HttpHeaders({
-      'Access-Control-Allow-Methods': 'DELETE, POST, GET, OPTIONS',
-      'Access-Control-Allow-Headers':
-        'Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With',
-      'Content-Type': 'application/json',
-      Authorization:
-        'ShippoToken shippo_test_56f0e027b06a2365334a11e35356731fdd51f825',
-    })},*/
-  }
-  /* validateAddress(address: Address) {
-    return this.http.get(
-      `${this.baseUrl}?address_line_1=${address.addressLine1}&address_line_2
-      =${address.addressLine2}&city_locality=${address.city}&state_province
-      =${address.state}&postal_code=${address.zip}&country_code=${address.country}`,
-      this.httpOptions
-    );
-  }*/
+  constructor(private http: HttpClient) {}
 
   saveAddress(address: Address) {
     console.log('In Service to call API');
